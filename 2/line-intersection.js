bfe676f2a1614c005ec04f820845de033e039908
Adam Pearce <1wheel@gmail.com>
2015-10-23 02:22:00 -0400

    loop

--- 2/line-intersection.js
+++ 2/line-intersection.js
@@ -1,26 +1,35 @@
 var w = h = 500
 
-var n = 50
-
-var points = d3.range(n).map(function(d){
-	return P(Math.random()*w, Math.random()*h)
-})
-
-
-var lines = points.map(function(p, i){
-	return [p, points[(i + 1) % points.length]]
-})
-
+var n = 200
 
 var m = 10
 var svg = d3.select('#graph').append('svg')
 		.attr({width: 500 + 2*m, height: 500 + 2*m})
 	.append('g').translate([m, m])
 
-render()
+
+var points, lines
+function setup(){
+	points = d3.range(n).map(function(d){
+		return P(Math.random()*w, Math.random()*h)
+	})
 
 
-d3.select(window).on('click', function(){
+	lines = points.map(function(p, i){
+		return [p, points[(i + 1) % points.length]]
+	})
+
+	render()	
+}
+setup()
+
+
+d3.select(window).on('click', step)
+
+
+
+
+function step(){
 	var badPoint = points.sort(d3.descendingKey(ƒ('intersections')))[0]
 	if (!badPoint.intersections) return
 
@@ -35,7 +44,8 @@ d3.select(window).on('click', function(){
 	points = points.filter(function(d){ return d != badPoint })
 
 	render()
-})
+
+}
 
 
 
@@ -58,23 +68,27 @@ function render(){
 
 
 	var lineSel = svg.selectAll('.line').data(lines, JSON.stringify)
-	lineSel.exit().classed('removed', true)
+	lineSel.exit().classed('removed', true).remove()
 	lineSel.enter().append('path.line').attr('d', function(d){
 		return ['M', d[0], 'L', d[1]].join(' ')
 	})
 
 	
 	var circleSel = svg.selectAll('.point').data(points, JSON.stringify)
-	circleSel.exit().classed('removed', true)
+	circleSel.exit().classed('removed', true).remove()
 	circleSel.enter().append('circle.point')
 			.translate(ƒ())
 			.attr('r', 5)
 
 	var intSel = svg.selectAll('.intersect').data(intersections, JSON.stringify)
-	intSel.exit().classed('removed', true)
+	intSel.exit().classed('removed', true).remove()
 	intSel.enter().append('circle.intersect')
 			.style({fill: 'red'})
 			.attr('r', 3)
 			.translate(ƒ())	
+
+
+	if (intersections.length) setTimeout(step, 0)
+	else setTimeout(setup, 1000)
 }
 
