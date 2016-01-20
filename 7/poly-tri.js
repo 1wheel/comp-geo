8a6a4d9085d28d789bcab4e0b09c9069d923f2e5
Adam Pearce <1wheel@gmail.com>
2016-01-20 03:07:33 -0500

    triangulation working

--- 7/poly-tri.js
+++ 7/poly-tri.js
@@ -18,7 +18,7 @@ svg.append('rect').attr({width: w, height: h, opacity: 0})
 points =  [[454,239],[630,126],[164,187],[266,459],[611,341],[488,318],[388,336],[287,324]].map(P)
 initDraw()
 
-
+trianglulate(points)
 
 
 function pointsToPoly(points){
@@ -31,13 +31,45 @@ function pointsToPoly(points){
 }
 
 function trianglulate(points){
-  if (points.length == 3) return
+  if (points.length < 4) return
+  console.log(points.length)
 
   var poly = pointsToPoly(points)
 
   var leftMost = _.min(poly, ƒ('x'))
 
-  
+  //points to draw line between
+  var u = leftMost.prev
+  var v = leftMost.next
+
+  var pointsInside = poly.filter(function(d){
+    return triangleContains(leftMost, leftMost.prev, leftMost.next, d) })
+  if (pointsInside.length){
+    var u = leftMost
+    var v = _.max(pointsInside, function(d){
+      return distLine(leftMost.prev, leftMost.next, d) })
+  }
+
+  svg.append('path').attr('d', 'M' + v + 'L' + u).style({stroke: '#f0f', fill: 'none'})
+
+  var points2 = [u]
+  var nextPoint = u
+  while (nextPoint != v){
+    nextPoint = nextPoint.next
+    points2.push(nextPoint)
+  }
+  var points3 = [v]
+  while (nextPoint != u){
+    nextPoint = nextPoint.next
+    points3.push(nextPoint)
+  }
+
+  if (!pointsInside.length) debugger
+
+
+  trianglulate(points2)
+  trianglulate(points3)
+
 }
 
 
