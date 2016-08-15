609d6fae99aa6ce9204717c372af85c7b7255a72
Adam Pearce <1wheel@gmail.com>
2016-08-15 01:30:18 -0400

    labeling points

--- examples/vertex-type/script.js
+++ examples/vertex-type/script.js
@@ -28,19 +28,24 @@ var polygonSel = svg.append('path')
 var lineSel = svg.append('g')
 
 var circleSel = svg.append('g')
+var textSel = svg.append('g')
 
 var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 function render(){
-  vertexType(points)
+  addVertexType(points)
 
   circleSel.html('').appendMany(points, 'circle.point')
       .at({r})
       .call(drag)
       .call(d3.attachTooltip)
       .translate(ƒ())
-      .st('fill', function(d){ return d.type == 'turn' ? '#0b0' : '#000' })
+      .st('fill', function(d){ return d.type == 'merge' || d.type == 'split' ? '#c00' : '#000'})
 
+  textSel.html('').appendMany(points, 'text.point')
+      .translate(ƒ())
+      .text(ƒ('type'))
+      .at({textAnchor: 'middle', dy: -10})
 
 
   polygonSel.at('d', pathStr)
@@ -51,15 +56,33 @@ render()
 
 
 
-function vertexType(pts){
+function addVertexType(pts){
   pts.forEach(function(d, i){
     var lP = pts[mod(i - 1, pts.length)]
     var rP = pts[mod(i + 1, pts.length)]
 
-    console.log(d[1] < rP[1])
-    console.log(d[1], lP[1])
-    d.type = d[1] < lP[1] ^ d[1] < rP[1] ? 'regular' : 'turn'
+    var isAboveL = d[1] < lP[1] || (d[1] == lP[1] && d[0] < lP[0])
+    var isAboveR = d[1] < rP[1] || (d[1] == rP[1] && d[0] < rP[0])
+    var isLeftPoint = isLeft(lP, d, rP)
+
+    if (isAboveL && isAboveR){
+      d.type = !isLeftPoint ? 'start' : 'split'
+    } else if (!isAboveL && !isAboveR){
+      d.type = !isLeftPoint ? 'end' : 'merge'
+    } else {
+      d.type = 'regular'
+    }
   })
 
-
 }
+
+function isLeft(a, b, c){
+  var ax = a[0], ay = a[1],
+      bx = b[0], by = a[1],
+      cx = c[0], cy = c[1]
+
+  return (bx - ax)*(cy - ay) - (by - ay)*(cx - ax) > 0
+}
+
+
+
